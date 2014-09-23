ab2ecb92b0d7b933e034220bcda40b5ab2019814
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2014-09-23 17:39:28 +0300

    Date time constants now have their own help page.

--- R/compound-constants.R
+++ R/compound-constants.R
@@ -1,20 +1,50 @@
-#' @rdname ALPHA
+#' @rdname CharacterClasses
 #' @include concatenation.R
 #' @include class-groups.R
 #' @export
 ASCII_ALPHA <- ASCII_LOWER %c% ASCII_UPPER
 
-#' @rdname ALPHA
+#' @rdname CharacterClasses
 #' @export
 ASCII_ALNUM <- ASCII_ALPHA %c% ASCII_DIGIT
 
-#' @rdname ALPHA
+#' ISO date-time constants
+#' 
+#' Compound regex constants for matching ISO 8601 dates and times.
+#' @seealso \code{\link{ClassGroups}}
+#' @examples 
+#' ISO_DATE
+#' ISO_TIME
+#' ISO_DATETIME
+#' dates <- seq(as.Date("2000-01-01"), as.Date("2001-01-01"), "1 day")
+#' datetimes <- seq(as.POSIXct(Sys.Date()), as.POSIXct(Sys.Date() + 1), "1 sec")
+#' times <- substring(datetimes, 12, 19)
+#' stopifnot(
+#'   all(grepl(ISO_DATE, dates)),
+#'   all(grepl(ISO_TIME, times)),
+#'   all(grepl(ISO_DATETIME, datetimes))
+#' )
+#' non_dates <- c(
+#'   "2000-13-01", "2000-01-32", "2000-00-01", "2000-01-00"
+#' )
+#' non_times <- c(
+#'   "24:00:00", "23:60:59", "23:59:62", "23 59 59"
+#' )
+#' stopifnot(
+#'   all(!grepl(ISO_DATE, non_dates)),
+#'   all(!grepl(ISO_TIME, non_times))
+#' )
+#' @name DateTimeConstants
+NULL
+
+#' @rdname DateTimeConstants
 #' @export
-ISO_DATE <- ascii_digit(4) %c%
+ISO_DATE <- token(
+  ascii_digit(4) %c%
   "-" %c%
   token(
     "1" %c% range(0, 2) %|% 
-      optional("0") %c% ascii_digit()
+      optional("0") %c% range(1, 9)
   ) %c%
   "-" %c%
   token(
@@ -22,12 +52,14 @@ ISO_DATE <- ascii_digit(4) %c%
       group("12") %c% ascii_digit() %|% 
       "3" %c% group("01")
   )
+)
 
-#' @rdname ALPHA
+#' @rdname DateTimeConstants
 #' @export
 ISO_TIME <- token(
-  group("01") %c% ascii_digit() %|% "2" %c% range("0", "3") 
-) %c% 
+  token(
+    group("01") %c% ascii_digit() %|% "2" %c% range("0", "3") 
+  ) %c% 
   ":" %c%
   range(0, 5) %c% ascii_digit() %c% 
   ":" %c%
@@ -35,7 +67,8 @@ ISO_TIME <- token(
     range(0, 5) %c% ascii_digit() %|%
       "6" %c% group("01") #leap seconds
   )
+)
 
-#' @rdname ALPHA
+#' @rdname DateTimeConstants
 #' @export
-ISO_DATETIME <- token(iso_date() %c% group(" T") %c% iso_time())
+ISO_DATETIME <- token(ISO_DATE %c% group(" T") %c% ISO_TIME)
