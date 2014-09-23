86df4eb14210327d551a14874878ac1af94d27d2
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2014-09-23 16:02:40 +0300

    Stricter checking for ISO_DATE.

--- R/compound-constants.R
+++ R/compound-constants.R
@@ -10,13 +10,18 @@ ASCII_ALNUM <- ASCII_ALPHA %c% ASCII_DIGIT
 
 #' @rdname ALPHA
 #' @export
-ISO_DATE <- ascii_digit(4) %c% 
+ISO_DATE <- ascii_digit(4) %c%
   "-" %c%
-  range(0, 1) %c%
-  ascii_digit() %c% 
+  token(
+    "1" %c% range(0, 2) %|% 
+      optional("0") %c% ascii_digit()
+  ) %c%
   "-" %c%
-  range(0, 3) %c%
-  ascii_digit()
+  token(
+    "0" %c% range(1, 9) %|% 
+      group("12") %c% ascii_digit() %|% 
+      "3" %c% group("01")
+  )
 
 #' @rdname ALPHA
 #' @export
