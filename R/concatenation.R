05407cfc7b26938ada220f0ccbb5c50ef3d32c42
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2014-10-28 17:56:58 +0300

    Minor link fix to avoid warning

--- R/concatenation.R
+++ R/concatenation.R
@@ -1,11 +1,11 @@
 #' Combine strings together
 #' 
-#' Operator equivalent of \code{\link[base]{paste0}} without \code{collapse}.
+#' Operator equivalent of \code{paste0} without \code{collapse}.
 #' 
 #' @param x A character vector.
 #' @param y A character vector.
 #' @return A character vector representing part or all of a regular expression.
-#' @seealso \code{\link[base]{paste0}}
+#' @seealso \code{\link[base]{paste}}
 #' @examples
 #' letters %c% LETTERS
 #' @export
