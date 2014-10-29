8f3b6b067d3b5e936c26bf54c4eb5e156b4d015f
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2014-10-29 16:54:19 +0300

    Extra datetime conversion formats supported.  Added documentation and reordered code.

--- R/datetime.R
+++ R/datetime.R
@@ -1,3 +1,88 @@
+#' Date-time regexes
+#' 
+#' Compound regex constants for matching ISO 8601 dates and times.
+#' @param x A \code{\link[base]{strptime}}-style date-time format string.
+#' @param locale A string specifying a locale.
+#' @seealso \code{\link{ClassGroups}}, \code{\link[base]{Sys.setlocale}}
+#' @examples 
+#' datetime("%m/%d/%Y")             # match US style dates
+#' datetime(token("%H" %|% "%I%p")) # match hours in 24h or 12h format
+#' 
+#' \dontrun{
+#' # week days and months are can be matched in any locale
+#' if(.Platform$OS.type == "windows")
+#' {
+#'   fr_FR <- "French_France"
+#'   ar_QA <- "Arabic_Qatar"
+#' } else 
+#' {
+#'   fr_FR <- "fr_FR.utf8"
+#'   ar_QA <- "ar_QA.utf8"
+#' }
+#' datetime("%a %A %b %B", fr_FR)
+#' datetime("%a %A %b %B", ar_QA)
+#' 
+#' # All letter tokens.  Lots of output.
+#' x <- paste0("%", c(letters, LETTERS))
+#' setNames(datetime(x), x)
+#' }
+#' 
+#' # Individual date-time components
+#' DTSEP             # optional selected punctuation or space
+#' CENTURY           # exactly two digits
+#' YEAR              # one to four digits
+#' YEAR2             # exactly two digits
+#' YEAR4             # exactly four digits
+#' MONTH             # number from 1 to 12, leading zero
+#' WEEK_OF_YEAR      # number from 0 to 53, leading zero
+#' DAY               # number from 1 to 31, leading zero
+#' DAY_SINGLE        # optional leading space
+#' HOUR24            # 24 hour clock, leading zero
+#' HOUR12            # 12 hour clock, leading zero
+#' HOUR24_SINGLE     # 24 hour clock, optional leading space
+#' HOUR12_SINGLE     # 12 hour clock, optional leading space
+#' MINUTE            # number from 0 to 59, leading zero
+#' SECOND            # number from 0 to 61 (leap seconds), leading zero
+#' FRACTIONAL_SECOND # a second optional decimal point and up to 6 digits
+#' AM_PM             # AM or PM, any case
+#' TIMEZONE_OFFSET   # optional plus or minus, then four digits
+#' TIMEZONE          # Any value returned by OlsonNames()
+#' # ISO 8601 formats
+#' ISO_DATE          # %Y-%m-%d
+#' ISO_TIME          # %H:%M:%S
+#' ISO_DATETIME      # ISO_DATE followed by ISO_TIME, separated by space or "T".
+#' # Compound forms, separated by DTSEP
+#' YMD
+#' YDM
+#' MYD
+#' MDY
+#' DYM
+#' DMY
+#' HMS
+#' HM
+#' MS
+#' 
+#' dates <- seq(as.Date("2000-01-01"), as.Date("2001-01-01"), "1 day")
+#' datetimes <- seq(as.POSIXct(Sys.Date()), as.POSIXct(Sys.Date() + 1), "1 sec")
+#' times <- substring(datetimes, 12, 19)
+#' stopifnot(
+#'   all(grepl(ISO_DATE, dates)),
+#'   all(grepl(ISO_TIME, times)),
+#'   all(grepl(ISO_DATETIME, datetimes))
+#' )
+#' non_dates <- c(
+#'   "2000-13-01", "2000-01-32", "2000-00-01", "2000-01-00"
+#' )
+#' non_times <- c(
+#'   "24:00:00", "23:60:59", "23:59:62", "23 59 59"
+#' )
+#' stopifnot(
+#'   all(!grepl(ISO_DATE, non_dates)),
+#'   all(!grepl(ISO_TIME, non_times))
+#' )
+#' @name DateTime
+NULL
+
 #' Get the days of the week
 #' 
 #' Get the names of the days of the week in a given locale.
@@ -44,6 +129,7 @@ get_weekdays <- function(abbreviate = FALSE, locale = NULL)
 
 get_weekdays_windows <- function(abbreviate = FALSE, locale = NULL)
 {
+  # TODO: Maybe worth reimplementing devtools::with_locale to clean this up.
   if(!is.null(locale))
   {
     lc_ctype <- Sys.getlocale("LC_CTYPE")
@@ -56,6 +142,8 @@ get_weekdays_windows <- function(abbreviate = FALSE, locale = NULL)
   # 20 days is likely overkill, but there may be weird locales with long weeks.
   days <- seq(Sys.Date(), Sys.Date() + 20, "1 day")
   weekday_names <- weekdays(days, abbreviate)
+  
+  # TODO: Is charset <- localeToCharset(Sys.getlocale("LC_TIME"))[1] platform independent?
   current_codepage <- as.character(l10n_info()$codepage)
   unique(
     iconv(weekday_names, from = current_codepage, to = "utf8")
@@ -118,108 +206,210 @@ get_months_linux <- function(abbreviate = FALSE, locale = NULL)
   unique(months(days, abbreviate))
 }
 
+#' @rdname DateTime
+#' @export
+DTSEP <- optional(group("-/.:,\\ "))
+
+#' @rdname DateTime
+#' @export
 CENTURY <- ascii_digit(2)
 
+#' @rdname DateTime
+#' @export
+YEAR <- ascii_digit(1, 4)
+
+#' @rdname DateTime
+#' @export
+YEAR2 <- ascii_digit(2)
+
+#' @rdname DateTime
+#' @export
+YEAR4 <- ascii_digit(4)
+
+#' @rdname DateTime
+#' @export
+MONTH <- token(
+  "1" %c% char_range(0, 2) %|% 
+    optional("0") %c% char_range(1, 9)
+)
+
+#' @rdname DateTime
+#' @export
+WEEK_OF_YEAR <- token(
+  "5" %c% char_range(0, 3) %|%
+    char_range(0, 4) %c% ascii_digit()
+)
+
+#' @rdname DateTime
+#' @export
 DAY <- token(
   "0" %c% char_range(1, 9) %|% 
     group("12") %c% ascii_digit() %|% 
     "3" %c% group("01")
 )
 
-EDAY <- token(
+#' @rdname DateTime
+#' @export
+DAY_SINGLE <- token(
   " " %c% char_range(1, 9) %|% 
     group("12") %c% ascii_digit() %|% 
     "3" %c% group("01")
 )
 
-HOUR <- token(
+#' @rdname DateTime
+#' @export
+DAY_OF_YEAR <- token(
+  "36" %c% char_range(0, 6) %|%
+    "3" %c% char_range(0, 5) %c% ascii_digit() %|%
+    optional(char_range(0, 2)) %c% optional(ascii_digit()) %c% ascii_digit()
+)
+
+#' @rdname DateTime
+#' @export
+WEEKDAY1 <- char_range(1, 7)
+
+#' @rdname DateTime
+#' @export
+WEEKDAY0 <- char_range(0, 6)
+
+#' @rdname DateTime
+#' @export
+HOUR24 <- token(
   group("01") %c% ascii_digit() %|% "2" %c% char_range("0", "3") 
 )
 
-IHOUR <- token(
-  group("0") %c% ascii_digit() %|% "1" %c% char_range("0", "2") 
+#' @rdname DateTime
+#' @export
+HOUR12 <- token(
+  "0" %c% ascii_digit() %|% "1" %c% char_range("0", "2") 
 )
 
-DAY_OF_YEAR <- token(
-  "36" %c% char_range(0, 6) %|%
-  "3" %c% char_range(0, 5) %c% ascii_digit() %|%
-  optional(char_range(0, 2)) %c% optional(ascii_digit()) %c% ascii_digit()
+#' @rdname DateTime
+#' @export
+HOUR24_SINGLE <- token(
+  optional(group(" 1")) %c% ascii_digit() %|% "2" %c% char_range("0", "3") 
 )
 
-MONTH <- token(
-  "1" %c% char_range(0, 2) %|% 
-    optional("0") %c% char_range(1, 9)
+#' @rdname DateTime
+#' @export
+HOUR12_SINGLE <- token(
+  optional(" ") %c% ascii_digit() %|% "1" %c% char_range("0", "2") 
 )
 
+#' @rdname DateTime
+#' @export
 MINUTE <- char_range(0, 5) %c% ascii_digit()
 
-AM_PM <- token("am" %|% "AM" %|% "pm" %|% "PM")
-
+#' @rdname DateTime
+#' @export
 SECOND <- token(
   char_range(0, 5) %c% ascii_digit() %|%
     "6" %c% group("01") #leap seconds
 )
 
-UWEEKDAY <- char_range(1, 7)
+#' @rdname DateTime
+#' @export
+FRACTIONAL_SECOND <- SECOND %c% optional(token(group(".,") %c% ascii_digit(1, 6)))
 
-WEEK_OF_YEAR <- token(
-  "5" %c% char_range(0, 3) %|%
-  char_range(0, 4) %c% ascii_digit()
-)
+#' @rdname DateTime
+#' @export
+AM_PM <- token("am" %|% "AM" %|% "pm" %|% "PM")
 
-WWEEKDAY <- char_range(0, 6)
-
-YEAR2 <- ascii_digit(2)
-
-YEAR4 <- ascii_digit(1, 4)
-
+#' @rdname DateTime
+#' @export
 TIMEZONE_OFFSET <- optional(group("-+")) %c% ascii_digit(4)
 
-TIMEZONE <- token(or1(OlsonNames()))
+#' @rdname DateTime
+#' @export
+TIMEZONE <- token(escape_special(or1(OlsonNames())))
 
-MDY <- MONTH %c% "/" %c% DAY %c% "/" %c% YEAR2
 
-FYMD <- YEAR4 %c% "-" %c% MONTH %c% "-" %c% DAY
+#' @rdname DateTime
+#' @export
+ISO_DATE <- YEAR4 %c% "-" %c% MONTH %c% "-" %c% DAY
 
-XYMD <- YEAR2 %c% "/" %c% MONTH %c% "/" %c% DAY
+#' @rdname DateTime
+#' @export
+ISO_TIME <- HOUR24 %c%  ":" %c% MINUTE %c%  ":" %c% SECOND
 
-HM <- HOUR %c% ":" %c% MINUTE
+#' @rdname DateTime
+#' @export
+ISO_DATETIME <- ISO_DATE %c% group(" T") %c% ISO_TIME
 
-HMS <- HOUR %c% ":" %c% MINUTE %c% ":" %c% SECOND
 
+#' @rdname DateTime
+#' @export
+YMD <- YEAR %c% MONTH %c% DTSEP %c% DAY %c% DTSEP
+
+#' @rdname DateTime
+#' @export
+YDM <- YEAR %c% DAY %c% DTSEP %c% MONTH %c% DTSEP
+
+#' @rdname DateTime
+#' @export
+MYD <- MONTH %c% DTSEP %c% YEAR %c% DTSEP %c% DAY
+
+#' @rdname DateTime
+#' @export
+MDY <- MONTH %c% DTSEP %c% DAY %c% DTSEP %c% YEAR
+
+#' @rdname DateTime
+#' @export
+DYM <- DAY %c% DTSEP %c% YEAR %c% DTSEP %c% MONTH
+
+#' @rdname DateTime
+#' @export
+DMY <- DAY %c% DTSEP %c% MONTH %c% DTSEP %c% YEAR
+
+#' @rdname DateTime
+#' @export
+HMS <- HOUR24 %c% DTSEP %c% MINUTE %c% DTSEP %c% SECOND
+
+#' @rdname DateTime
+#' @export
+HM <- HOUR24 %c% DTSEP %c% MINUTE
+
+#' @rdname DateTime
+#' @export
+MS <- MINUTE %c% DTSEP %c% SECOND
+
+
+#' @rdname DateTime
+#' @export
 datetime <- function(x, locale = NULL)
 {
   x <- gsub("%c", "%a %b %e %H:%M:%S %Y", x)
+  x <- gsub("%\\+", "%a %b %e %H:%M:%S %Z %Y", x)
   x <- gsub("%a", get_weekdays(TRUE, locale = locale), x)
   x <- gsub("%A", get_weekdays(locale = locale), x)
   x <- gsub("%[bh]", get_months(TRUE, locale = locale), x)
   x <- gsub("%B", get_months(locale = locale), x)
   x <- gsub("%C", CENTURY, x)
-  x <- gsub("%d", DAY, x)
-  x <- gsub("%D", MDY, x)
-  x <- gsub("%e", EDAY, x)
-  x <- gsub("%F", FYMD, x)
-  x <- gsub("%H", HOUR, x)
-  x <- gsub("%[Ir]", IHOUR, x)
+  x <- gsub("%O?d", DAY, x)
+  x <- gsub("%D", MONTH %c% "/" %c% DAY %c% "/" %c% YEAR2, x)
+  x <- gsub("%e", DAY_SINGLE, x)
+  x <- gsub("%F", ISO_DATE, x)
+  x <- gsub("%O?H", HOUR24, x)
+  x <- gsub("%O?[Ir]", HOUR12, x)
   x <- gsub("%j", DAY_OF_YEAR, x)
-  x <- gsub("%m", MONTH, x)
-  x <- gsub("%M", MINUTE, x)
+  x <- gsub("%k", HOUR24_SINGLE, x)
+  x <- gsub("%l", HOUR12_SINGLE, x)
+  x <- gsub("%O?m", MONTH, x)
+  x <- gsub("%O?M", MINUTE, x)
   x <- gsub("%[nt]", space(0), x)
+  x <- gsub("%OS", FRACTIONAL_SECOND, x)
   x <- gsub("%[pP]", AM_PM, x)
-  x <- gsub("%R", HM, x)
+  x <- gsub("%R", HOUR24 %c% ":" %c% MINUTE, x)
+  x <- gsub("%s", ascii_digit(1, 17), x)
   x <- gsub("%S", SECOND, x)
-  x <- gsub("%[TX]", HMS, x)
-  x <- gsub("%u", UWEEKDAY, x)
-  x <- gsub("%[gGUVW]", WEEK_OF_YEAR, x)
-  x <- gsub("%w", WWEEKDAY, x)
-  x <- gsub("%x", XYMD, x)
-  x <- gsub("%y", YEAR2, x)
+  x <- gsub("%[TX]", ISO_TIME, x)
+  x <- gsub("%u", WEEKDAY1, x)
+  x <- gsub("%O?[gGUVW]", WEEK_OF_YEAR, x)
+  x <- gsub("%O?w", WEEKDAY0, x)
+  x <- gsub("%x", YEAR2 %c% "/" %c% MONTH %c% "/" %c% DAY, x)
+  x <- gsub("%O?y", YEAR2, x)
   x <- gsub("%Y", YEAR4, x)
   x <- gsub("%z", TIMEZONE_OFFSET, x)
   x <- gsub("%Z", TIMEZONE, x)
-  # TODO %k, %l, %s, %+, %O, %E, %O1, ...%O6
-  
   x
 }
-
-# cat(datetime(paste0("%", c(letters, LETTERS), collapse = "\n")))
