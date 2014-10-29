555cac3debb78336b2b4f0f64ac2a221cc04db7d
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2014-10-29 10:23:59 +0300

    Added or1 function.

--- R/alternation.R
+++ R/alternation.R
@@ -6,11 +6,16 @@
 #' @param y A character vector.
 #' @param ... Character vectors.
 #' @return A character vector representing part or all of a regular expression.
+#' @note \code{or} takes multiple character vector inputs and returns a 
+#' character vector of the inputs separated by pipes. \code{\%|\%} is an operator 
+#' interface to this function. \code{or1} takes a single character vector and 
+#' returns a string collapsed by pipes.
 #' @seealso \code{\link[base]{paste}}
 #' @references \url{http://www.regular-expressions.info/alternation.html}
 #' @examples
 #' or(letters, LETTERS, "foo")
 #' letters %|% LETTERS %|% "foo"
+#' or1(letters)
 #' @export
 or <- function(...)
 {
@@ -20,7 +25,7 @@ or <- function(...)
       "or is intended to be called with at least 2 arguments. ",
       nargs(),
       ngettext(nargs(), " was", " were"),
-      " passed."
+      " passed. Maybe you wanted or1 instead?"
     )
   }
   paste(..., sep = "|", collapse = NULL)  
@@ -31,4 +36,11 @@ or <- function(...)
 `%|%` <- function(x, y)
 {
   or(x, y)
+}
+
+#' @rdname or
+#' @export
+or1 <- function(x)
+{
+  paste0(x, collapse = "|")
 }
