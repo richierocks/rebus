30297ed801e8d4dc84178fc8e19a2df39c4c01e8
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-01-19 17:50:40 +0300

    added perl-regex methods

--- R/regex-methods.R
+++ R/regex-methods.R
@@ -1,16 +1,28 @@
 
 #' Convert or test for regex objects
 #' 
-#' \code{as.regex} gives objects the class \code{"regex"}. \code{is.regex} tests
-#' for objects of class \code{"regex"}.
+#' \code{as.regex} gives objects the class \code{"regex"}. \code{is.regex}
+#' tests for objects of class \code{"regex"}.
 #' @param x An object to test or convert.
 #' @return \code{as.regex} returns the inputs object, with class 
 #' \code{c("regex", "character")}.
 #' \code{is.regex} returns \code{TRUE} when the input inherits from class 
 #' \code{"regex"} and \code{FALSE} otherwise.
+#' Similarly \code{as.perl_regex} gives the input object a class of
+#' \code{c("perl-regex", "regex", "character")}.  For compatibility with
+#' the \code{stringr} package it also sets the attribute \code{perl = TRUE}.
+#' @examples
+#' x <- as.regex("month.abb")
+#' is.regex(x)
+#' as.perl_regex("month.abb")
+#' is.perl_regex(lookahead("a"))
 #' @export
 as.regex <- function(x)
 {
+  if(is.regex(x))
+  {
+    return(x)
+  }
   structure(x, class = c("regex", "character"))
 }
 
@@ -23,12 +35,16 @@ is.regex <- function(x)
 
 #' Create a regex
 #' 
-#' Creates a regex object.
+#' Creates a regex object.  
 #' @param ... Passed to \code{paste0}.
 #' @return An object of class \code{regex}.
 #' @note This works like \code{paste0}, but the returns value has class
 #' \code{c("regex", "character")}.
-#' @seealso \code{\link[base]{paste0}}, \code{\link{as.regex}}
+#' @seealso \code{\link[base]{paste0}}
+#' as.regex(month.abb)
+#' regex(letters[1:5], "?")
+#' as.perl_regex(month.abb)
+#' perl_regex(letters[1:5], "?")
 #' @export
 regex <- function(...)
 {
@@ -42,17 +58,54 @@ regex <- function(...)
 #' @param ... Passed from other format methods.  Currently ignored.
 #' @return \code{format.regex} returns a character vector. \code{print.regex}
 #' is invoked for the side effect of printing the regex object.
-#' @note Backslashes are escaped by the print method so it looks like what the 
-#' regular expression engine sees.
+#' @examples
+#' group(1:5)
+#' lookahead(1:5)
 #' @export
-format.regex = function(x, ...)
+format.regex <- function(x, ...)
 {
   paste0("<regex> ", x) 
 }
 
 #' @rdname format.regex
 #' @export
-print.regex = function(x, ...)
+print.regex <- function(x, ...)
 {
   cat(format(x, ...), sep = "\n")
+}
+
+#' @rdname as.regex
+#' @export
+as.perl_regex <- function(x)
+{
+  if(is.perl_regex(x))
+  {
+    return(x)
+  }
+  structure(
+    x, 
+    class = c("perl-regex", "regex", "character"), 
+    perl  = TRUE
+  )
+}
+
+#' @rdname as.regex
+#' @export
+is.perl_regex <- function(x)
+{
+  inherits(x, "perl-regex")
+}
+
+#' @rdname regex
+#' @export
+perl_regex <- function(...)
+{
+  as.perl_regex(paste0(...))
+}
+
+#' @rdname regex
+#' @export
+`format.perl-regex` <- function(x, ...)
+{
+  paste0("<perl-regex> ", x) 
 }
