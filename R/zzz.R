d6a31a2273f885981b00753f9ba741770de404e3
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-01-20 15:55:32 +0300

    New startup message.

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname)
 {
-  msg <- "Some renaming to more correctly represent standard regular expression terminology has occured since v0.0-1.  The group function now creates non-captured groups (replacing token).  Be careful: this change breaks old code!  Sorry.  The original group function has been renamed char_class.  negate_and_group has been renamed negated_char_class."
+  msg <- "%R% is now preferred over %c% for joining regexes, since it is easier to type."
   packageStartupMessage(paste(strwrap(msg), collapse = "\n"))  
 }
