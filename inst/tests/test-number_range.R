d06dcfb956af91589324590eb508d2a9b96757b2
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-02-04 11:32:44 +0300

    added tests for number_range

---
+++ inst/tests/test-number_range.R
@@ -0,0 +1,47 @@
+test_that(
+  "number_range returns a correct regex for 0 to 9999",
+  {
+    expected <- structure(
+      "(?:[0-9]{4})",
+      class = c("regex", "character")
+    )
+    actual <- number_range(0, 9999)
+    expect_equal(actual, expected)
+  }  
+)
+
+test_that(
+  "number_range returns a correct regex for 123 to 876",
+  {
+    expected <- structure(
+      "(?:12[3-9]|1[3-9][0-9]|[2-7][0-9]{2}|8[0-6][0-9]|87[0-6])",
+      class = c("regex", "character")
+    )
+    actual <- number_range(123, 876)
+    expect_equal(actual, expected)
+  }  
+)
+
+test_that(
+  "number_range returns a correct regex for 1010 to 9090",
+  {
+    expected <- structure(
+      "(?:10[1-9][0-9]|1[1-9][0-9]{2}|[2-8][0-9]{3}|90[0-8][0-9]|9090)",
+      class = c("regex", "character")
+    )
+    actual <- number_range(1010, 9090)
+    expect_equal(actual, expected)
+  }  
+)
+
+test_that(
+  "number_range returns a correct regex for -123 to 876",
+  {
+    expected <- structure(
+      "(?:-(?:[1-9]|[1-9][0-9]|1[0-1][0-9]|12[0-3])|(?:[0-7][0-9]{2}|8[0-6][0-9]|87[0-6]))",
+      class = c("regex", "character")
+    )
+    actual <- number_range(-123, 876)
+    expect_equal(actual, expected)
+  }  
+)
