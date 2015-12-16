508ac2e2d79611e28e5f694ab9112511215337e8
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-12-16 11:30:18 +0300

    outsource content to rebus.base

--- R/zzz.R
+++
@@ -1,5 +0,0 @@
-.onAttach <- function(libname, pkgname)
-{
-  msg <- "%R% is now preferred over %c% for joining regexes, since it is easier to type."
-  packageStartupMessage(paste(strwrap(msg), collapse = "\n"))  
-}
